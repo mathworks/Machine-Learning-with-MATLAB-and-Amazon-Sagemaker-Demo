9f3434f004f68e080568b86078a72adeb4334bc2
Richard Alcock <richard.alcock@gmail.com>
2023-09-22 11:04:51 +0000

    try embed images

--- buildfile.m
+++ buildfile.m
@@ -7,8 +7,7 @@ plan("clean") = CleanTask;
 
 plan("markdown").Inputs = "**/*.mlx";
 plan("markdown").Outputs = replace(plan("markdown").Inputs, ".mlx",".md");
-plan("markdown").Outputs(end+1) = replace(plan("markdown").Inputs, ".mlx","_media");
-plan("markdown").Outputs(end+1) = "mdOutputs.txt";
+
 
 plan("jupyter").Inputs = "**/*.mlx";
 plan("jupyter").Outputs = replace(plan("jupyter").Inputs, ".mlx",".ipynb");
@@ -22,14 +21,13 @@ mlxFiles = ctx.Task.Inputs.paths;
 mdFiles = ctx.Task.Outputs(1).paths;
 for idx = 1:numel(mlxFiles) 
     disp("Building markdown file from " + mlxFiles(idx))
-    export(mlxFiles(idx), mdFiles(idx), Run=false, EmbedImages=false, Caching=false);
+    export(mlxFiles(idx), mdFiles(idx), Run=false, Caching=false);
 end
 
-writelines(ctx.Task.Outputs.paths,"mdOutputs.txt");
 end
 
 function jupyterTask(ctx)
-% Generate jupyer notebooks from all mlx files
+% Generate jupyter notebooks from all mlx files
 
 mlxFiles = ctx.Task.Inputs.paths;
 ipynbFiles = ctx.Task.Outputs.paths;
